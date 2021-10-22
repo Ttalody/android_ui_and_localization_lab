b5e716bcda162e3588de29f7d2d3537e7eb1f870
Artur Akulich <59253474+Ttalody@users.noreply.github.com>
2021-10-22 15:09:49 +0300

    Add files via upload

---
+++ lab_resource_localiztion_layout/app/src/main/java/com/example/lab_resource_localiztion_layout/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.lab_resource_localiztion_layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
